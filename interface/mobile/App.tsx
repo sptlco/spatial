a8ae4b3af8d12d95c353aca02d0e3b08c1f8e3ea
Dakarai Cundiff <karai@sptlco.com>
2025-09-22 19:09:02 +0000

    fix: removed background color

--- interface/mobile/App.tsx
+++ interface/mobile/App.tsx
@@ -7,9 +7,9 @@ import "./global.css"
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className="flex-1 items-center justify-center bg-background">
-        <Text className="text-3xl font-bold text-white">
-          Welcome.
+      <SafeAreaView className="flex-1 items-center justify-center">
+        <Text className="text-3xl font-bold">
+          Welcome to Spatial
         </Text>
         <StatusBar style="auto"/>
       </SafeAreaView>

303421ff889fb3a7a57efbed0df8a65fbe5d2f35
Dakarai Cundiff <karai@sptlco.com>
2025-09-22 01:14:41 -0700

    fix: Update route.ts

--- api/app/route.ts
+++ api/app/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
 
 export const GET = async () => {
-    return NextResponse.json({app: "spatial-api"});
+    return NextResponse.json({app: "spatial"});
 }

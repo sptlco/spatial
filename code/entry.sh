cb956dfdce9806831f95b174f0ebda4da2aa89c8
Dakarai Cundiff <karai@sptlco.com>
2025-09-24 14:48:06 -0700

    fix: remove redundant arguments

--- code/entry.sh
+++ code/entry.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-exec code-server --bind-addr 0.0.0.0:8080 \
-    --auth password \
+exec code-server ${PROJECT_DIR} \
     --user-data-dir "${DATA_DIR}" \
-    --config "${CONFIG_DIR}/config.yaml" \
-    "${PROJECT_DIR}"
+    --config "${CONFIG_DIR}/config.yaml"

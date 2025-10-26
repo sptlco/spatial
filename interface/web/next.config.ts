2f7f155c84668e82b7fb98aed0950228e57edd4e
Dakarai Cundiff <karai@sptlco.com>
2025-10-26 08:25:24 -0700

    fix: resolve paths (#51)

--- interface/web/next.config.ts
+++ interface/web/next.config.ts
@@ -1,7 +1,15 @@
 // Copyright © Spatial Corporation. All rights reserved.
 
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    webpack(config) {
+        config.resolve.alias["@sptlco/matter"] = path.resolve(__dirname, "../design");
+        config.resolve.alias["@sptlco/matter/*"] = path.resolve(__dirname, "../design/*");
+
+        return config;
+    }
+};
 
 export default nextConfig;

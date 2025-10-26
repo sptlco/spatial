456780e602bec9566c833d8cc073796e2882bf54
Dakarai Cundiff <karai@sptlco.com>
2025-10-26 12:05:47 -0700

    fix: update workspaces (#51)

--- interface/web/next.config.ts
+++ interface/web/next.config.ts
@@ -2,6 +2,10 @@
 
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        externalDir: true
+    }
+};
 
 export default nextConfig;

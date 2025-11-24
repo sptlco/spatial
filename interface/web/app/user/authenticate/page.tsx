1ee616634e21b0da2af28f40d3bd2663974107e0
Dakarai Cundiff <karai@sptlco.com>
2025-11-24 22:15:42 +0000

    fix: Use alpha for design elements (#51)

--- interface/web/app/user/authenticate/page.tsx
+++ interface/web/app/user/authenticate/page.tsx
@@ -8,16 +8,14 @@ import { Button, Field, Form, Icon, Main, Span } from "@sptlco/matter";
  */
 export default function Authentication() {
   return (
-    <Main className="flex items-center justify-center h-screen">
+    <Main className="flex items-center justify-center h-screen bg-background-surface">
       <Form className="flex flex-col w-full max-w-md space-y-12">
         <Field type="text" label="Email Address" placeholder="you@sptlco.com" description="This is your account's email address." />
         <Field type="password" label="Passphrase" placeholder="This is my passphrase" description="This is your account's email address." />
         <Field type="text" label="Sigma" placeholder="Enter a sigma value" description="This is your account's email address." />
-        <Button size="small" shape="pill" intent="primary">
+        <Button className="w-full" intent="secondary" shape="pill">
           <Span>Continue</Span>
-        </Button>
-        <Button intent="secondary" shape="pill">
-          <Span>Continue</Span>
+          <Icon symbol="arrow_right_alt" />
         </Button>
       </Form>
     </Main>

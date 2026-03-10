7f785525707fa8bd9b98e1b0cb75bb3b16484afc
Dakarai Cundiff <karai@sptlco.com>
2026-03-10 15:18:51 +0000

    feat: Added a shipment editor (#137)

--- interface/web/app/[locale]/platform/logistics/shipments/shipments.tsx
+++ interface/web/app/[locale]/platform/logistics/shipments/shipments.tsx
@@ -4,11 +4,12 @@
 
 import { clsx } from "clsx";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
+
+import { Button, Container, createElement, Field, Form, Icon, LI, Pagination, Separator, Sheet, Span, UL } from "@sptlco/design";
 
 import { Application } from "@/elements";
 
-import { Button, Container, createElement, Field, Form, Icon, LI, Pagination, Separator, Span, UL } from "@sptlco/design";
 import { View, views } from "./view";
 import { Filters } from "./filters";
 import { Sort } from "./sort";
@@ -122,66 +123,25 @@ export const Shipments = createElement<typeof Application.Root>((props, ref) => 
 
   const render = () => {
     switch (view) {
-      case "grid":
+      case "grid": {
         return (
           <UL className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10">
             {shipments.map((shipment, i) => {
-              return [...Array(5)].map((_, j) => (
-                <LI key={`${i}+${j}`} className="flex flex-col bg-input outline outline-line-faint rounded-4xl">
-                  <Container className="flex flex-col p-10 xl:gap-2">
-                    <Container className="flex items-center justify-between">
-                      <Span className="text-2xl xl:text-4xl font-bold">{shipment.id}</Span>
-                      <Button intent="ghost" className="p-0! size-10!">
-                        <Icon symbol="more_horiz" className="font-light" />
-                      </Button>
-                    </Container>
-                    <Span className="text-foreground-quaternary text-xs uppercase">2 Parcels • 2 Units</Span>
-                  </Container>
-                  <Separator className="flex w-full h-px bg-line-faint" />
-                  {renderAddress("Origin", shipment.from)}
-                  <Separator className="flex w-full h-px bg-line-faint" />
-                  {renderAddress("Destination", shipment.to)}
-                </LI>
-              ));
+              return [...Array(5)].map((_, j) => {
+                const [editing, setEditing] = useState(false);
+
+                return (
+                  <Fragment key={`${i}+${j}`}>
+                    <Shipment.Card onClick={() => setEditing(true)} shipment={shipment} />
+                    <Shipment.Editor open={editing} onOpenChange={setEditing} shipment={shipment} onEdit={(_) => {}} />
+                  </Fragment>
+                );
+              });
             })}
           </UL>
         );
+      }
     }
-  };
-
-  const renderAddress = (title: string, address: Address) => {
-    return (
-      <Container className="flex flex-col p-10 gap-5">
-        <Span className="flex items-center gap-2.5 text-lg xl:text-2xl font-bold">
-          <Span>{title}</Span>
-          <Span className="text-foreground-quaternary font-normal">
-            {address.city}, {address.state}
-          </Span>
-        </Span>
-        <UL className="flex flex-wrap gap-5 xl:gap-10 w-full">
-          {address.name && (
-            <LI className="flex flex-col mr-auto">
-              <Span className="text-sm mb-2 text-foreground-quaternary">Name</Span>
-              <Span className="truncate">{address.name}</Span>
-            </LI>
-          )}
-          {address.company && (
-            <LI className="flex flex-col mr-auto">
-              <Span className="text-sm mb-2 text-foreground-quaternary">Company</Span>
-              <Span className="truncate">{address.company}</Span>
-            </LI>
-          )}
-          <LI className="flex flex-col col-start-3">
-            <Span className="text-sm mb-2 text-foreground-quaternary">Address</Span>
-            <Span className="truncate">{address.line1}</Span>
-            {address.line2 && <Span className="truncate">{address.line2}</Span>}
-            <Span className="truncate">
-              {address.city}, {address.state} {address.zip}
-            </Span>
-          </LI>
-        </UL>
-      </Container>
-    );
   };
 
   return (
@@ -210,9 +170,7 @@ export const Shipments = createElement<typeof Application.Root>((props, ref) => 
                 }
               }}
             />
-
             <Icon symbol="search" className="absolute left-3" />
-
             {keywords.length > 0 ? (
               <Button
                 type="button"
@@ -250,3 +208,68 @@ export const Shipments = createElement<typeof Application.Root>((props, ref) => 
     </Application.Root>
   );
 });
+
+const Shipment = {
+  Address: createElement<typeof Container, { title: string; address: Address }>(({ title, address, ...props }, ref) => {
+    return (
+      <Container {...props} ref={ref} className="flex flex-col p-10 gap-5">
+        <Span className="flex items-center gap-2 text-lg xl:text-2xl font-bold">
+          <Span>{title}</Span>
+          <Span className="text-foreground-quaternary font-normal">
+            {address.city}, {address.state}
+          </Span>
+        </Span>
+        <UL className="flex flex-wrap gap-5 xl:gap-10 w-full">
+          {address.name && (
+            <LI className="flex flex-col mr-auto">
+              <Span className="text-sm mb-2 text-foreground-quaternary">Name</Span>
+              <Span className="truncate">{address.name}</Span>
+            </LI>
+          )}
+          {address.company && (
+            <LI className="flex flex-col mr-auto">
+              <Span className="text-sm mb-2 text-foreground-quaternary">Company</Span>
+              <Span className="truncate">{address.company}</Span>
+            </LI>
+          )}
+          <LI className="flex flex-col col-start-3">
+            <Span className="text-sm mb-2 text-foreground-quaternary">Address</Span>
+            <Span className="truncate">{address.line1}</Span>
+            {address.line2 && <Span className="truncate">{address.line2}</Span>}
+            <Span className="truncate">
+              {address.city}, {address.state} {address.zip}
+            </Span>
+          </LI>
+        </UL>
+      </Container>
+    );
+  }),
+
+  Separator: createElement<typeof Separator>((props, ref) => <Separator {...props} ref={ref} className="flex w-full h-px bg-line-faint" />),
+
+  Card: createElement<typeof Button, { shipment: Shipment }>(({ shipment, ...props }, ref) => {
+    return (
+      <LI className="flex flex-col bg-input outline outline-line-faint rounded-4xl">
+        <Button {...props} ref={ref} intent="none" size="fit" className="flex flex-col items-start w-full p-10 xl:gap-2">
+          <Container className="flex w-full items-center justify-between">
+            <Span className="text-2xl xl:text-4xl font-bold">{shipment.id}</Span>
+            <Icon symbol="arrow_right_alt" className="font-light" />
+          </Container>
+          <Span className="text-foreground-quaternary text-xs uppercase">2 Parcels • 2 Units</Span>
+        </Button>
+        <Shipment.Separator />
+        <Shipment.Address title="Origin" address={shipment.from} />
+        <Shipment.Separator />
+        <Shipment.Address title="Destination" address={shipment.to} />
+      </LI>
+    );
+  }),
+
+  Editor: createElement<typeof Sheet.Root, { shipment: Shipment; onEdit: (shipment: Shipment) => void }>(({ shipment, onEdit, ...props }, ref) => {
+    return (
+      <Sheet.Root {...props} ref={ref}>
+        <Sheet.Content>{shipment.id}</Sheet.Content>
+      </Sheet.Root>
+    );
+  })
+};

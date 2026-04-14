368f76ebfe43d8f8f584fed0d9da304447293189
Dakarai Cundiff <karai@sptlco.com>
2026-04-14 06:13:41 +0000

    feat: Convert PYUSD to USDC

--- cloud/Core/Constants.cs
+++ cloud/Core/Constants.cs
@@ -227,6 +227,11 @@ internal class Constants
         public const string SwapExactTokensForETH = "swapExactTokensForETH";
 
         /// <summary>
+        /// A function that lets you swap ERC20 tokens for ERC20 tokens.
+        /// </summary>
+        public const string SwapExactTokensForTokens = "swapExactTokensForTokens";
+
+        /// <summary>
         /// A function that lets you get expected output token amounts from a swap.
         /// </summary>
         public const string GetAmountsOut = "getAmountsOut";
